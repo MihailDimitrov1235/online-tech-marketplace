560efd0369768203f127f7fdd79cd5deff8f7387
Miroslav Balev <m.balev@editcreation.com>
2026-03-16 16:47:20 +0200

    add alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,12 @@ import packageJson from "./package.json" with { type: "json" }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "src"),
+    },
+  },
 
   server: {
     open: true,
